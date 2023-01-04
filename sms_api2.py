089d9b97ffcd1795ccf9f4123ab6349edafc0073
ariyo123 <89997464+ariyo123@users.noreply.github.com>
2023-01-05 00:43:39 +0100

    Update sms_api2.py

--- sms_api2.py
+++ sms_api2.py
@@ -1,12 +1,12 @@
 import requests
 
 # Your Account SID and Auth Token from twilio.com/console
-account_sid = 'AC950d6468759c4f5d16cf5b9b052baaad'
-auth_token = 'e219c55a5bfb128aea3c8593a19e65f2'
-phones=['+23436363667','+2347033377186']
+account_sid = 'sid'
+auth_token = 'token'
+phones=['+23436363667','+23455555555']
 for phone in phones[:]:
 # The phone number you want to send the SMS from, and the phone number you want to send it to
-    from_number = '+12058394287'
+    from_number = '+3445555'
     to_number = phone
 
     # The body of the SMS message
